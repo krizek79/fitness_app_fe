825a6085569490e9bed9312afe18be1dbbd90c22
krizek79 <krizan.matej79@gmail.com>
2024-06-26 00:37:55 +0200

    updated libraries and started working on workout update tabs

--- src/components/page/workout/WorkoutDetailEdit.tsx
+++ src/components/page/workout/WorkoutDetailEdit.tsx
@@ -9,6 +9,7 @@ import {
 } from "../../../schema/WorkoutUpdateRequest"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react"
 
 export default function WorkoutDetailEdit() {
     const { getWorkoutLoading, workout } = useGetWorkout()
@@ -40,8 +41,8 @@ export default function WorkoutDetailEdit() {
 
     const handleTextareaBlur = (e: FocusEvent<HTMLTextAreaElement>) => {
         const textarea = e.target
-        textarea.rows = 1
         textarea.style.height = "auto"
+        textarea.rows = 1
     }
 
     function onSubmit(data: WorkoutUpdateRequest) {
@@ -60,81 +61,143 @@ export default function WorkoutDetailEdit() {
                     }
                 >
                     <GoBackButton />
-                    <h1 className={"text-text font-bold text-2xl"}>General</h1>
-                    {getWorkoutLoading && <Loading />}
-                    {workout && (
-                        <div className={"flex flex-col gap-y-3"}>
-                            <div className="flex flex-col">
-                                <label
-                                    htmlFor="name"
-                                    className={"text-base text-secondary"}
+                    <TabGroup className={"border-b"}>
+                        <TabList className={"flex w-full justify-between"}>
+                            <Tab
+                                key={"general"}
+                                className="py-3 text-text text-lg w-full flex justify-center focus:outline-none data-[selected]:bg-accent data-[hover]:bg-accent transition duration-150"
+                            >
+                                General
+                            </Tab>
+                            <Tab
+                                key={"exercises"}
+                                className="py-3 text-text text-lg w-full flex justify-center focus:outline-none data-[selected]:bg-accent data-[hover]:bg-accent transition duration-150"
+                            >
+                                Exercises
+                            </Tab>
+                        </TabList>
+                        <TabPanels className={"w-full py-6"}>
+                            <TabPanel key={"general"} className={"w-full"}>
+                                <form
+                                    className={"flex flex-col gap-y-6 w-full"}
                                 >
-                                    Name
-                                </label>
-                                <input
-                                    id={"name"}
-                                    name={"name"}
-                                    type={"text"}
-                                    className={
-                                        "w-full py-1.5 border-b-2 placeholder-transparent focus:outline-none " +
-                                        "focus:border-primary bg-background text-gray-500 focus:text-text"
-                                    }
-                                    value={workout.name}
-                                />
-                            </div>
-                            <div className="flex flex-col">
-                                <label className="text-base text-secondary">
-                                    Level
-                                </label>
-                                <ul className="w-full py-1.5 flex flex-wrap flex-col gap-y-1.5 sm:flex-row sm:gap-x-6">
-                                    {workoutLevels &&
-                                        workoutLevels.map((level) => (
-                                            <li
-                                                key={level.key}
-                                                className="flex gap-x-3 items-center"
-                                            >
-                                                <input
-                                                    type="radio"
-                                                    id={level.key}
-                                                    name="level"
-                                                    value={level.key}
-                                                    className="w-5 h-5"
-                                                />
+                                    {getWorkoutLoading && <Loading />}
+                                    {workout && (
+                                        <div
+                                            className={"flex flex-col gap-y-3"}
+                                        >
+                                            <div className="flex flex-col">
                                                 <label
-                                                    htmlFor={level.key}
-                                                    className="text-text"
+                                                    htmlFor="name"
+                                                    className={
+                                                        "text-base text-secondary"
+                                                    }
                                                 >
-                                                    {level.value}
+                                                    Name
                                                 </label>
-                                            </li>
-                                        ))}
-                                </ul>
-                            </div>
-                            <div className="flex flex-col">
-                                <label
-                                    htmlFor="description"
-                                    className={"text-base text-secondary"}
+                                                <input
+                                                    id={"name"}
+                                                    name={"name"}
+                                                    type={"text"}
+                                                    className={
+                                                        "w-full py-1.5 border-b-2 placeholder-transparent focus:outline-none " +
+                                                        "focus:border-primary bg-background text-gray-500 focus:text-text"
+                                                    }
+                                                    value={workout.name}
+                                                />
+                                            </div>
+                                            <div className="flex flex-col">
+                                                <label className="text-base text-secondary">
+                                                    Level
+                                                </label>
+                                                <ul className="w-full py-1.5 flex flex-wrap flex-col gap-y-1.5 sm:flex-row sm:gap-x-6">
+                                                    {workoutLevels &&
+                                                        workoutLevels.map(
+                                                            (level) => (
+                                                                <li
+                                                                    key={
+                                                                        level.key
+                                                                    }
+                                                                    className="flex gap-x-3 items-center"
+                                                                >
+                                                                    <input
+                                                                        type="radio"
+                                                                        id={
+                                                                            level.key
+                                                                        }
+                                                                        name="level"
+                                                                        value={
+                                                                            level.key
+                                                                        }
+                                                                        className="w-5 h-5"
+                                                                    />
+                                                                    <label
+                                                                        htmlFor={
+                                                                            level.key
+                                                                        }
+                                                                        className="text-text"
+                                                                    >
+                                                                        {
+                                                                            level.value
+                                                                        }
+                                                                    </label>
+                                                                </li>
+                                                            )
+                                                        )}
+                                                </ul>
+                                            </div>
+                                            <div className="flex flex-col">
+                                                <label
+                                                    htmlFor="description"
+                                                    className={
+                                                        "text-base text-secondary"
+                                                    }
+                                                >
+                                                    Description
+                                                </label>
+                                                <textarea
+                                                    id={"description"}
+                                                    name={"description"}
+                                                    ref={textareaRef}
+                                                    className={
+                                                        "w-full py-1.5 border-b-2 placeholder-transparent focus:outline-none " +
+                                                        "focus:border-primary bg-background text-gray-500 focus:text-text " +
+                                                        "overflow-y-hidden resize-none"
+                                                    }
+                                                    value={workout.description}
+                                                    rows={1}
+                                                    onClick={
+                                                        handleTextareaClick
+                                                    }
+                                                    onChange={
+                                                        handleTextareaChange
+                                                    }
+                                                    onBlur={handleTextareaBlur}
+                                                />
+                                            </div>
+                                        </div>
+                                    )}
+                                    <button
+                                        disabled={true}
+                                        className={
+                                            "flex w-full justify-center gap-x-3 items-center border px-6 py-1.5 " +
+                                            "transition ease-in-out bg-gradient-to-r from-primary to-accent hover:bg-gradient-to-br " +
+                                            "duration-150 disabled:opacity-50 disabled:cursor-not-allowed"
+                                        }
+                                    >
+                                        Save
+                                    </button>
+                                </form>
+                            </TabPanel>
+                            <TabPanel key={"exercises"} className={"w-full"}>
+                                <form
+                                    className={"flex flex-col gap-y-6 w-full"}
                                 >
-                                    Description
-                                </label>
-                                <textarea
-                                    id={"description"}
-                                    name={"description"}
-                                    ref={textareaRef}
-                                    className={
-                                        "w-full py-1.5 border-b-2 placeholder-transparent focus:outline-none " +
-                                        "focus:border-primary bg-background text-gray-500 focus:text-text " +
-                                        "overflow-y-hidden resize-none"
-                                    }
-                                    value={workout.description}
-                                    rows={1}
-                                    onClick={handleTextareaClick}
-                                    onChange={handleTextareaChange}
-                                    onBlur={handleTextareaBlur}
-                                />
-                            </div>
-                        </div>
-                    )}
+                                    Something...
+                                </form>
+                            </TabPanel>
+                        </TabPanels>
+                    </TabGroup>
                 </div>
             </div>
         </>
